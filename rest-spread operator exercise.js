9de5614f783d78c5ed06d1464fd89fbb9c72e99e
tomoss18 <111102060+tomoss18@users.noreply.github.com>
2022-08-23 22:46:16 -0700

    Add files via upload

---
+++ rest-spread operator exercise.js
@@ -0,0 +1,62 @@
+function filterOutOdds() {
+    var nums = Array.prototype.slice.call(arguments);
+    return nums.filter(function(num) {
+      return num % 2 === 0
+    });
+  }
+
+const filterOutOdds = (...args) => args.filter(v => v % 2 ===0)
+
+const findMin = (...args) => Math.min(...args)
+
+const mergObjects = (obj1, obj2) => ({...obj1, ...obj2})
+
+const doubleAndReturnArgs = (arr, ...args) => [...arr, ...args.map(v => v *2)]
+
+/** remove a random element in the items array
+and return a new array without that item. */
+
+const removeRandom = items => {
+    let idx = Math.floor(Math.random() * items.length);
+    return [...items.slice(0, idx), ...items.slice(idx + 1)];
+}
+
+/** Return a new array with every item in array1 and array2. */
+
+const extend = (array1, array2) => {
+    return [...array1, ...array2];
+}
+
+/** Return a new object with all the keys and values
+from obj and a new key/value pair */
+
+const addKeyVal = (obj, key, val) => {
+    let newObj = {...obj}
+    newObj[key] = val;
+    return newObj;
+}
+
+
+/** Return a new object with a key removed. */
+
+const removeKey = (obj, key) => {
+    let newObj = {...obj}
+    delete newObj[key]
+    return newObj;
+}
+
+
+/** Combine two objects and return a new object. */
+
+const combine = (obj1, obj2) => {
+    return { ...obj1, ...obj2};
+}
+
+
+/** Return a new object with a modified key and value. */
+
+const update = (obj, key, val) => {
+    let newObj = {...obj}
+    newObj[key] = val;
+    return newObj;
+}
